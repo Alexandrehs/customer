d87cedf424d3e702f2f2b9f3fd9a280794becbb3
AlexandreHS <echo.alexandre@gmail.com>
2021-04-03 14:33:11 -0300

    Remoção de tudo do banco, e padronizaoção dos nomes dos atributos

--- src/main/kotlin/br/com/estudante/customerapi/repository/CustomerRepository.kt
+++
@@ -1,8 +0,0 @@
-package br.com.estudante.customerapi.repository
-
-import br.com.estudante.customerapi.entity.CustomerRequest
-import org.springframework.data.jpa.repository.JpaRepository
-
-
-interface CustomerRepository : JpaRepository<CustomerRequest, Long> {
-}
